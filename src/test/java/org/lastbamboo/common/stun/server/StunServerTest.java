9048c8af715efc6fa2d63b5d32dc16c1d4415e7e
adamfisk <a@littleshoot.org>
2007-05-17 17:33:14 +0000

    Cleaned up
    git-svn-id: http://svn.littleshoot.org/svn/littleshoot/trunk/common/stun/server@405 8d7aff80-242d-0410-bcee-ed6f15385b3d

--- src/test/java/org/lastbamboo/common/stun/server/StunServerTest.java
+++ src/test/java/org/lastbamboo/common/stun/server/StunServerTest.java
@@ -4,28 +4,14 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 
 import junit.framework.TestCase;
-import net.java.stun4j.NetAccessPointDescriptor;
 import net.java.stun4j.StunAddress;
 import net.java.stun4j.StunException;
 import net.java.stun4j.client.MappedAddressListener;
-import net.java.stun4j.client.NetworkConfigurationDiscoveryProcess;
-import net.java.stun4j.client.StunDiscoveryReport;
+import net.java.stun4j.client.SimpleAddressDetector;
 import net.java.stun4j.stack.StunStack;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.mina.common.ByteBuffer;
-import org.apache.mina.common.ConnectFuture;
-import org.apache.mina.common.IoConnector;
-import org.apache.mina.common.IoHandler;
-import org.apache.mina.common.IoSession;
-import org.apache.mina.transport.socket.nio.DatagramConnector;
-import org.lastbamboo.common.stun.stack.message.StunMessage;
-import org.lastbamboo.common.stun.stack.message.StunMessageFactory;
-import org.lastbamboo.common.stun.stack.message.StunMessageFactoryImpl;
-import org.lastbamboo.common.stun.stack.message.attributes.StunAttributesFactory;
-import org.lastbamboo.common.stun.stack.message.attributes.StunAttributesFactoryImpl;
-import org.lastbamboo.common.util.NetworkUtils;
 
 /**
  * Tests the STUN server.
@@ -39,39 +25,10 @@ public class StunServerTest extends TestCase implements MappedAddressListener
         {
         final StunLauncher launcher = new StunLauncher();
         launcher.launch();
-        //final StunAttributesFactory attributesFactory = 
-          //  new StunAttributesFactoryImpl();
-        
-        //final StunMessageFactory messageFactory = 
-          //  new StunMessageFactoryImpl(attributesFactory);
-        
-        //final StunServer server = new StunServerImpl(messageFactory);
         
         LOG.debug("Server started...running clients...");
         
         hitServer();
-        /*
-        final IoConnector connector = new DatagramConnector();
-        final InetSocketAddress stunServer = 
-            new InetSocketAddress(NetworkUtils.getLocalHost(), 3478);
-        
-        final IoHandler handler = new StunClientIoHandler();
-        final ConnectFuture future = connector.connect(stunServer, handler);
-        
-        future.join();
-        final IoSession session = future.getSession();
-        
-        final StunMessage message = messageFactory.createBindingRequest();
-        
-        final ByteBuffer buffer = ByteBuffer.wrap("Test this out\r\n".getBytes());
-        session.write(buffer);
-        */
-        
-        //session.getCloseFuture().join();
-        
-        //acceptor.unbind( address );
-        
-        //Thread.sleep(12000);
         }
  
     /**
@@ -90,35 +47,28 @@ public class StunServerTest extends TestCase implements MappedAddressListener
         // discover the NAT type and not because we need the mapped address.
         final int port = 8111;
         
-        final StunAddress localAddress = 
-            new StunAddress(NetworkUtils.getLocalHost(), port);
-        final NetAccessPointDescriptor desc =
-            new NetAccessPointDescriptor(localAddress);
-        
         final StunStack stack = new StunStack();
         
-        final StunAddress serverAddress = 
-            new StunAddress("127.0.0.1", 3478);
-        final NetworkConfigurationDiscoveryProcess netConfigDiscovery =
-            new NetworkConfigurationDiscoveryProcess(desc, serverAddress, stack);
-        netConfigDiscovery.start();
+        // Switch to the central server if you want to test remotely.
+        //final String server = "lastbamboo.org";
+        final String server = "127.0.0.1";
+        final StunAddress serverAddress = new StunAddress(server, 3478);
         
-        final StunDiscoveryReport report = 
-            netConfigDiscovery.determineAddress(this);
-        LOG.trace(report.getNatType());
+        final SimpleAddressDetector detector = 
+            new SimpleAddressDetector(serverAddress, stack);
         
-        final StunAddress stunAddress = report.getPublicAddress();
+        final StunAddress stunAddress = detector.getMappingFor(port);
         
         if (stunAddress != null)
             {
             // The address can be null if we're behind a UDP blocking 
             // firewall and therefore have no public address.
-            final InetSocketAddress publicAddress = 
-                report.getPublicAddress().getSocketAddress();
-            //LOG.debug("Found public address in STUN report: " + 
-              //  this.m_publicAddress);
-            //final Preferences prefs = Preferences.userRoot();
-            //prefs.put(PrefKeys.PUBLIC_URL, this.m_publicAddress.getHostName());
+            final InetSocketAddress publicAddress = stunAddress.getSocketAddress();
+            LOG.debug("Got MAPPED ADDRESS"+ publicAddress);
+            }
+        else
+            {
+            fail("Did not get address");
             }
         }
 
