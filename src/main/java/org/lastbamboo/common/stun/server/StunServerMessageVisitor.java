cefdc79cf8fd3835779972f23e8f8512a954ef08
adamfisk <a@littleshoot.org>
2007-07-31 21:38:40 +0000

    Simplified some visitors and config
    git-svn-id: http://svn.littleshoot.org/svn/littleshoot/trunk/common/stun/server@822 8d7aff80-242d-0410-bcee-ed6f15385b3d

--- src/main/java/org/lastbamboo/common/stun/server/StunServerMessageVisitor.java
+++ src/main/java/org/lastbamboo/common/stun/server/StunServerMessageVisitor.java
@@ -6,22 +6,24 @@ import org.apache.commons.id.uuid.UUID;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.mina.common.IoSession;
+import org.lastbamboo.common.stun.stack.message.BindingErrorResponse;
 import org.lastbamboo.common.stun.stack.message.BindingRequest;
 import org.lastbamboo.common.stun.stack.message.NullStunMessage;
 import org.lastbamboo.common.stun.stack.message.StunMessage;
 import org.lastbamboo.common.stun.stack.message.StunMessageVisitor;
-import org.lastbamboo.common.stun.stack.message.SuccessfulBindingResponse;
+import org.lastbamboo.common.stun.stack.message.BindingSuccessResponse;
+import org.lastbamboo.common.stun.stack.message.StunMessageVisitorAdapter;
 import org.lastbamboo.common.stun.stack.message.turn.AllocateRequest;
 import org.lastbamboo.common.stun.stack.message.turn.ConnectRequest;
 import org.lastbamboo.common.stun.stack.message.turn.ConnectionStatusIndication;
 import org.lastbamboo.common.stun.stack.message.turn.DataIndication;
 import org.lastbamboo.common.stun.stack.message.turn.SendIndication;
-import org.lastbamboo.common.stun.stack.message.turn.SuccessfulAllocateResponse;
+import org.lastbamboo.common.stun.stack.message.turn.AllocateSuccessResponse;
 
 /**
  * Class that visits read messages on a STUN server.
  */
-public class StunServerMessageVisitor implements StunMessageVisitor<Object>
+public class StunServerMessageVisitor extends StunMessageVisitorAdapter<Object>
     {
 
     private static final Log LOG = 
@@ -47,61 +49,9 @@ public class StunServerMessageVisitor implements StunMessageVisitor<Object>
         
         final UUID transactionId = binding.getTransactionId();
         final StunMessage response = 
-            new SuccessfulBindingResponse(transactionId.getRawBytes(), address);
+            new BindingSuccessResponse(transactionId.getRawBytes(), address);
         
         this.m_session.write(response);
         return null;
         }
-
-    public Object visitAllocateRequest(final AllocateRequest request)
-        {
-        LOG.debug("Received unhandled message on server: "+request);
-        return null;
-        }
-
-    public Object visitSendIndication(final SendIndication request)
-        {
-        LOG.debug("Received unhandled message on server: "+request);
-        return null;
-        }
-
-    public Object visitDataIndication(final DataIndication data)
-        {
-        LOG.debug("Received unhandled message on server: "+data);
-        return null;
-        }
-    
-    public Object visitConnectRequest(final ConnectRequest request)
-        {
-        LOG.debug("Received unhandled message on server: "+request);
-        return null;
-        }
-
-    public Object visitSuccessfulAllocateResponse(
-        final SuccessfulAllocateResponse response)
-        {
-        LOG.error("Unexpected message received on server: "+response);
-        return null;
-        }
-
-    public Object visitConnectionStatusIndication(
-        final ConnectionStatusIndication indication)
-        {
-        LOG.error("Unexpected message received on server: "+indication);
-        return null;
-        }
-    
-    public Object visitSuccessfulBindingResponse(
-        final SuccessfulBindingResponse response)
-        {
-        LOG.error("We should not get binding responses on the server");
-        return null;
-        }
-
-    public Object visitNullMessage(final NullStunMessage message)
-        {
-        LOG.error("Received null message on the server");
-        return null;
-        }
-
     }
